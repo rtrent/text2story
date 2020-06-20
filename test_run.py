46d6680316c14a7574245f46b2296895c83d0679
Ricky Trent <richard.trent22@gmail.com>
2020-06-20 19:49:16 -0400

    clean up image files

--- test_run.py
+++
@@ -1,5 +0,0 @@
-from note2story import convert
-
-convert(shape='story', bg_color='red', output_name='goodbye')
-
-convert(output_name='hello', input_text='what is going on folks?\n\n\n\n my name is ricky.')
